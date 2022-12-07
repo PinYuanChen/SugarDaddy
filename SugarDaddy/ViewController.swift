388a4f52fc337b94fda206ee23d3ac3b4c037f9a
Patrick <champion790713@gmail.com>
2022-12-07 14:11:12 +0800

    genesis

---
+++ SugarDaddy/ViewController.swift
@@ -0,0 +1,16 @@
+//
+// Created on 2022/12/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
