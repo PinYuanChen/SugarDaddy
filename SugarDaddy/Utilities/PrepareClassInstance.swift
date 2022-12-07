b7fcfbfe032aaf750b75f88de1b9d6ada7c68d84
PinYuan <>
2022-12-07 22:30:44 +0800

    @dynamicMemberLookup method chaining setup

--- SugarDaddy/Utilities/PrepareClassInstance.swift
+++ SugarDaddy/Utilities/PrepareClassInstance.swift
@@ -4,10 +4,35 @@
 
 import Foundation
 
+/// Prepare class instance
 infix operator -->
 
-/// Prepare class instance
 func --> <T>(object: T, closure: (T) -> Void) -> T {
     closure(object)
     return object
+}
+
+/// Setup properties via method chaining ( only available after Swift 5.1)
+/// Either use 'Make' to wrap up property or use postfix operator
+@dynamicMemberLookup
+struct Make<T> {
+    let object: T
+    
+    @inlinable public init(_ obj: T) {
+        self.object = obj
+    }
+    
+    subscript<Value>(dynamicMember keyPath: WritableKeyPath<T, Value>) -> ((Value) -> Make<T>) {
+        var obj = self.object
+        return { value in
+            obj[keyPath: keyPath] = value
+            return Make(obj)
+        }
+    }
+}
+
+postfix operator >>
+
+postfix func >> <T>(lhs: T) -> Make<T> {
+    return Make(lhs)
 }
